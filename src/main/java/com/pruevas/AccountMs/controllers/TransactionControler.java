b9f9ceb0bcf0e9a119e766f3d058ae391bc56759
titobriceno <titooswaldobriceno@gmail.com>
2021-11-24 15:35:37 -0500

    implement the transaction part 2

--- src/main/java/com/pruevas/AccountMs/controllers/TransactionControler.java
+++ src/main/java/com/pruevas/AccountMs/controllers/TransactionControler.java
@@ -1,7 +1,10 @@
 package com.pruevas.AccountMs.controllers;
+import com.pruevas.AccountMs.models.Transaction;
 import com.pruevas.AccountMs.repositories.AccountRepository;
 import com.pruevas.AccountMs.repositories.TransactionRepository;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class TransactionControler {
@@ -11,5 +14,15 @@ public class TransactionControler {
     public TransactionControler(TransactionRepository transactionRepository, AccountRepository accountRepository) {
         this.transactionRepository = transactionRepository;
         this.accountRepository = accountRepository;
+    }
+
+    @GetMapping("transactions/{UserNameOrigin}")
+    List<Transaction> getTransactionByUserNameOrigin(@PathVariable String userNameOrigin){
+        return transactionRepository.getByUsernameOrigin(userNameOrigin);
+    }
+
+    @PostMapping("/transaction/")
+    Transaction newTransaction(@RequestBody Transaction transaction){
+        return transactionRepository.save(transaction);
     }
 }
